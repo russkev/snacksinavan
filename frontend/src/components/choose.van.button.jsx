bd581974e9f7660944ba65d1b57732388b3255d1
russkev <russkev@gmail.com>
2022-01-24 19:44:26 +1100

    Added snack menu path to to of link

--- frontend/src/components/choose.van.button.jsx
+++ frontend/src/components/choose.van.button.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LoadingButton from "./loading.button";
 import useCart from "../hooks/useCart";
+import Routes from "../routes/routes";
 
 export default function ChooseVanButton(props) {
-  const {updateVan, vanChoiceLoading } = useCart();
-  function chooseVan(){
+  const { updateVan, vanChoiceLoading } = useCart();
+
+  const chooseVan = () => {
     updateVan(props.van.vanName);
-  }
-    return (
-      <LoadingButton isLoading={vanChoiceLoading}>
-        <Link
-          to="#"
-          onClick={() => chooseVan()}
-          className="primary button soft-shadow"
-        >
-          Select
-        </Link>
-      </LoadingButton>
-    );  
+  };
+
+  return (
+    <LoadingButton isLoading={vanChoiceLoading}>
+      <Link
+        to={Routes.SNACKS_MENU.path}
+        onClick={chooseVan}
+        className="primary button soft-shadow"
+      >
+        Select
+      </Link>
+    </LoadingButton>
+  );
 }
